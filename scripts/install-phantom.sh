46ddbf0c7b34e731d1b9f0ecc7cd9f6071672eb3
Marius <gbhrdt@users.noreply.github.com>
2020-10-01 10:44:29 +0200

    Update install-phantom.sh

--- scripts/install-phantom.sh
+++ scripts/install-phantom.sh
@@ -17,7 +17,7 @@ if [ "$INSTALL_PHANTOMJS" = true ]; then
 
   cd /tmp
   wget https://github.com/Medium/phantomjs/releases/download/v$PHANTOM_VERSION/$PHANTOM_JS.tar.bz2
-  tar xvjf $PHANTOM_JS.tar.bz2
+  tar xjf $PHANTOM_JS.tar.bz2
   mv $PHANTOM_JS /usr/local/share
   ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/share/phantomjs
   ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
